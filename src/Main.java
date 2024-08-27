869388bff40761edd166735db529caff5f12ed94
EbadShelby <ebadyasser123xd@gmail.com>
2024-08-27 15:50:14 +0800

    initial java

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        int rows;
+//        int cols;
+//        String symbol;
+//        System.out.print("Enter # of rows: ");
+//        rows = scanner.nextInt();
+//        System.out.print("Enter # of columns: ");
+//        cols = scanner.nextInt();
+//        System.out.print("Enter Symbol: ");
+//        symbol = scanner.next();
+//
+//        for (int i = 1; i <= rows; i++) {
+//            System.out.println();
+//            for (int j = 0; j < cols; j++) {
+//                System.out.print(symbol);
+//            }
+//        }
+
+        String[] fruits = {"apple", "banana", "pineapple"};
+    }
+}
